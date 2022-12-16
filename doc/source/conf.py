5ca687460864d4ba5b0951019e6146b806390e41
Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
2022-12-16 16:04:19 +0100

    DOC: enable multi-version by migrating to pyansys/actions (#39)
    * DOC: enable multi-version
    
    * CI: migrate to pyansys/actions
    
    * DOC: add pdf builder

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,7 +1,8 @@
 """Sphinx documentation configuration file."""
 from datetime import datetime
+import os
 
-from ansys_sphinx_theme import pyansys_logo_black
+from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 
 from ansys.additive import __version__
 
@@ -10,15 +11,22 @@ project = "ansys-additive"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS, Inc."
 release = version = __version__
+cname = os.getenv("DOCUMENTATION_CNAME", "nocname.com")
 
 # use the default pyansys logo
 html_logo = pyansys_logo_black
 html_theme = "ansys_sphinx_theme"
+html_favicon = ansys_favicon
 
 # specify the location of your github repo
 html_theme_options = {
     "github_url": "https://github.com/pyansys/pyadditive",
     "show_prev_next": False,
+    "switcher": {
+        "json_url": f"https://{cname}/release/versions.json",
+        "version_match": get_version_match(__version__),
+    },
+    "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
 }
 
 # Sphinx extensions
