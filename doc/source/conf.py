31b2bafc2c1b8b03a95398cf8d85f9747adca812
Peter Krull - ANSYS <peter.krull@ansys.com>
2023-02-28 09:11:54 -0700

    Fix json url definition (#61)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,7 +51,7 @@ html_theme_options = {
     "github_url": "https://github.com/pyansys/pyadditive",
     "show_prev_next": False,
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
     },
     "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
