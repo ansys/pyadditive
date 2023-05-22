c159960f61c9bbf7a16492f6e9b2279428982793
Peter Krull <peter.krull@ansys.com>
2023-05-22 17:48:57 -0600

    Use data frame for melt pool stats

--- examples/00_additive_single_bead.py
+++ examples/00_additive_single_bead.py
@@ -10,8 +10,6 @@ Units are SI (m, kg, s, K) unless otherwise noted.
 
 First, connect to the Additive service.
 """
-import matplotlib.pyplot as plt
-
 import ansys.additive as pyadditive
 
 additive = pyadditive.Additive()
@@ -20,7 +18,7 @@ additive = pyadditive.Additive()
 # Material Selection
 # ------------------
 # The next step is a to choose a material. A list of available materials can
-# be obtained using the ``get_materials_list`` command.
+# be obtained using the :meth:`ansys.additive.additive.get_materials_list` command.
 
 print(additive.get_materials_list())
 
@@ -31,7 +29,7 @@ material = additive.get_material("17-4PH")
 ###############################################################################
 # Machine Parameter Specification
 # -------------------------------
-# Specify machine parameters by first creating an ``AdditiveMachine`` object
+# Specify machine parameters by first creating an :class:`ansys.additive.machine.AdditiveMachine` object
 # then assigning the desired values. All values are in SI units (m, kg, s, K)
 # unless otherwise noted.
 
@@ -48,7 +46,7 @@ machine.laser_power = 300  # W
 ###############################################################################
 # Specify Single Bead Simulation Inputs
 # -------------------------------------
-# Create a ``SingleBeadInput`` object containing the desired simulation
+# Create a :class:`ansys.additive.single_bead.SingleBeadInput` object containing the desired simulation
 # parameters.
 
 input = pyadditive.SingleBeadInput(
@@ -58,25 +56,44 @@ input = pyadditive.SingleBeadInput(
 ###############################################################################
 # Run Simulation
 # --------------
-# Use the ``simulate`` method of the ``additive`` object to run the simulation.
+# Use the :meth:`ansys.additive.additive.Additive.simulate` method of the ``additive``
+# object to run the simulation. The returned object is a
+# :class:`ansys.additive.single_bead.SingleBeadSummary` containing the input and a
+# :class:`ansys.additive.single_bead.MeltPool` object.
 
 summary = additive.simulate(input)
 
 ###############################################################################
 # Plot Melt Pool Statistics
 # -------------------------
-# You can plot the melt pool statistics using matplotlib.
+# A ``Pandas DataFrame`` containing the melt pool statistics can be obtained
+# using the :meth:`ansys.additive.single_bead.MeltPool.data_frame` property of the
+# ``melt_pool`` attribute of the ``summary`` object. The ``DataFrame.plot`` method
+# can be used to plot the melt pool dimensions as a function of bead length.
 
-_, ax = plt.subplots()
-mp = summary.melt_pool
-ax.plot(mp.laser_x, mp.length, label="length")
-ax.plot(mp.laser_x, mp.width, label="width")
-ax.plot(mp.laser_x, mp.depth, label="depth")
-ax.plot(mp.laser_x, mp.reference_width, label="reference_width")
-ax.plot(mp.laser_x, mp.reference_depth, label="reference_depth")
-ax.legend()
-ax.set_xlabel("Bead Length (m)")
-ax.set_ylabel("Melt Pool (m)")
-ax.set_title("Melt Pool Statistics")
+df = summary.melt_pool.data_frame.multiply(1e6)  # convert from meters to microns
+df.index *= 1e3  # convert bead length from meters to millimeters
 
-plt.show()
+df.plot(
+    y=["length", "width", "depth", "reference_width", "reference_depth"],
+    ylabel="Melt Pool Dimensions (µm)",
+    xlabel="Bead Length (mm)",
+    title="Melt Pool Dimensions vs Bead Length",
+)
+
+
+###############################################################################
+# List Melt Pool Statistics
+# -------------------------
+# A table of the melt pool statistics can be obtained using one of various methods
+# of the ``DataFrame``.
+
+df.style.format("{:.2f}").format_index("{:.2f}")
+
+###############################################################################
+# Save Melt Pool Statistics
+# -------------------------
+# The melt pool statistics can be saved to a CSV file using the ``DataFrame.to_csv``
+# method.
+
+df.to_csv("melt_pool.csv", index_label="bead_length")
--- tests/test_single_bead.py
+++ tests/test_single_bead.py
@@ -15,20 +15,15 @@ def test_MeltPool_init_converts_MeltPoolMessage():
     melt_pool = MeltPool(test_utils.get_test_melt_pool_message())
 
     # assert
-    assert len(melt_pool.laser_x) == 1
-    assert len(melt_pool.laser_y) == 1
-    assert len(melt_pool.length) == 1
-    assert len(melt_pool.width) == 1
-    assert len(melt_pool.reference_width) == 1
-    assert len(melt_pool.depth) == 1
-    assert len(melt_pool.reference_depth) == 1
-    assert melt_pool.laser_x[0] == 1
-    assert melt_pool.laser_y[0] == 2
-    assert melt_pool.length[0] == 3
-    assert melt_pool.width[0] == 4
-    assert melt_pool.reference_width[0] == 5
-    assert melt_pool.depth[0] == 6
-    assert melt_pool.reference_depth[0] == 7
+    df = melt_pool.data_frame
+    assert df is not None
+    assert len(df.index) == 1
+    assert df.index[0] == 1
+    assert df.iloc[0]["length"] == 3
+    assert df.iloc[0]["width"] == 4
+    assert df.iloc[0]["reference_width"] == 5
+    assert df.iloc[0]["depth"] == 6
+    assert df.iloc[0]["reference_depth"] == 7
 
 
 def test_SingleBeadSummary_init_returns_valid_result():
@@ -210,16 +205,10 @@ def test_MeltPool_repr_returns_expected_string():
     mp = MeltPool(mp_msg)
 
     # act, assert
-    assert (
-        mp.__repr__()
-        == "MeltPool\n"
-        + "laser_x: [1.0]\n"
-        + "laser_y: [2.0]\n"
-        + "length: [3.0]\n"
-        + "width: [4.0]\n"
-        + "depth: [5.0]\n"
-        + "reference_width: [6.0]\n"
-        + "reference_depth: [7.0]\n"
+    assert mp.__repr__() == (
+        "MeltPool\n"
+        "     length  width  depth  reference_width  reference_depth\n"
+        "1.0     3.0    4.0    5.0              6.0              7.0"
     )
 
 
@@ -286,13 +275,7 @@ def test_SingleBeadSummary_repr_returns_expected_string():
         + "thermal_properties_data: ThermalPropertiesDataPoint[]\n"
         + "\n"
         + "melt_pool: MeltPool\n"
-        + "laser_x: []\n"
-        + "laser_y: []\n"
-        + "length: []\n"
-        + "width: []\n"
-        + "depth: []\n"
-        + "reference_width: []\n"
-        + "reference_depth: []\n"
+        + "Empty DataFrame\nColumns: [length, width, depth, reference_width, reference_depth]\nIndex: []"
         + "\n"
     )
 
